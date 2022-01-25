c60459d5890839f47c9ab2ad35b528968b570d3f
portuna85 <portuna85@gmail.com>
2022-01-25 21:55:17 +0900

    test

---
+++ src/quiz/oop1/ex02/Exercise6_4.java
@@ -0,0 +1,19 @@
+package quiz.oop1.ex02;
+
+public class Exercise6_4 {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.setName("홍길동");
+        s.setBan(1);
+        s.setNo(1);
+        s.setEng(60);
+        s.setKor(100);
+        s.setMath(70);
+
+        System.out.println("이름 : " + s.getName());
+        System.out.println("총점 : " + s.getTotal());
+        System.out.println("총점 : " + s.getAverage());
+
+    }
+}
